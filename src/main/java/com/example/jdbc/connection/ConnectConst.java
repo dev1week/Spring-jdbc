466c54ad32aa2f4649b841367ffae70061e97df5
dev1week <rondo2860@gmail.com>
2023-01-26 23:29:37 +0900

    jdbc 활용 CRUD 구현

---
+++ src/main/java/com/example/jdbc/connection/ConnectConst.java
@@ -0,0 +1,8 @@
+package com.example.jdbc.connection;
+
+public abstract class ConnectConst {
+    public static final String URL ="jdbc:h2:tcp://localhost/~/testdb";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+}
---
+++ src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,32 @@
+package com.example.jdbc.repository;
+
+import com.example.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV0Test {
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void save() throws SQLException {
+        Member member = new Member("memberV7", 100000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+
+        repository.update(member.getMemberId(), 200000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(200000);
+    }
+}
